446c03eefdd4346cf9f1373e48e56f78acc31d67
RASantos92 <rasbms81608@gmail.com>
2023-07-31 16:11:16 -0500

    7/31 afternoon lecture

--- Test.java
+++ Test.java
@@ -1,8 +1,46 @@
+import java.util.ArrayList;
 public class Test {
     public static void main(String[] args){
         VoiceMessageUtil messageUtil = new VoiceMessageUtil();
         System.out.println(messageUtil.greeting());
         System.out.println(messageUtil.greeting("Isaiah", 26));
         messageUtil.getCurrentDate();
+
+
+        // let testArray = [1,2,3,4];
+        int[] messagesPerDay = new int[4]; // [0,0,0,0]
+        messagesPerDay[0] = 5; // [5,0,0,0]
+        messagesPerDay[1] = 5;
+        messagesPerDay[2] = 5;
+
+        // int[] messagesPerDay1 = {5,5,5};
+        // messagesPerDay1[0] = 1;
+
+        String[] messages = {"String1" , "String2", "String3"};
+
+        System.out.println(messageUtil.totalMessages(messagesPerDay));
+
+        messageUtil.printMessages(messages);
+
+        ArrayList<String> todoList = new ArrayList<String>();
+        todoList.add("Finish Alfred Bot");
+        todoList.add("Finish Cafe Business logic");
+        todoList.add("Read ahead for oop");
+
+        messageUtil.printMessages(todoList);
+
+
+        int days = 7;
+        double costOfLivingPerDay = 500.66; 
+
+        //no loss of data
+        double totalCostOfLivingForAWeek = days * costOfLivingPerDay; // int => double
+
+        // possible we loose data
+        Integer totalCostOfLivingForAWeekInt = days * (int) costOfLivingPerDay; // double => int
+        System.out.println(totalCostOfLivingForAWeekInt);
+
+
+
     }
 }
--- VoiceMessageUtil.java
+++ VoiceMessageUtil.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.ArrayList;
 public class VoiceMessageUtil {
 
     public String greeting(){
@@ -17,4 +18,35 @@ public class VoiceMessageUtil {
         Date today = new Date();
         System.out.println(today);
     }
+
+    public int totalMessages(int[] intArr){
+        int sum = 0;
+        for(int i = 0; i<intArr.length ; i++){
+            sum += intArr[i];
+        }
+        return sum;
+    }
+
+    public void printMessages(String[] messages){
+        // for(int i = 0; i < messages.length; i++){
+        //     System.out.println(messages[i]);
+        // }
+
+        for(String message : messages){
+            System.out.println(message);
+        }
+
+    }
+
+    public void printMessages(ArrayList<String> messages){
+        // for(int i = 0 ; i<messages.size(); i++){
+        //     System.out.println(messages.get(i));
+        // }
+
+        for(String message : messages){
+            System.out.println(message);
+        }
+    }
+
+    
 }
